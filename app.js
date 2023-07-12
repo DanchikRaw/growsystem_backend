78d1078b6f3b50393103e5cd46eb67ee659fd551
dkraw <46016587+DanchikRaw@users.noreply.github.com>
2023-07-12 19:07:05 +0700

    fix port

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const mqtt = require('mqtt')
 
 const app = express()
+const port = process.env.PORT || process.env.SERVER_PORT;
 
 
 app.get('/', async (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Example app listening on port ${process.env.SERVER_PORT}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
